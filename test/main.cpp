fffa1758617a030e0977037677a883a1af08cdc0
Rokthewok <jqruffer@gmail.com>
2017-08-20 19:14:41 -0400

    Add gtest skeleton files

---
+++ test/main.cpp
@@ -0,0 +1,8 @@
+//
+// Created by john on 8/20/17.
+//
+#include <gtest/gtest.h>
+
+int main(int argc, char ** argv) {
+    return 0;
+}
---
+++ test/test_eventqueue.cpp
@@ -0,0 +1,9 @@
+//
+// Created by john on 8/20/17.
+//
+#include <gtest/gtest.h>
+
+
+TEST(EventQueueTests, TestConstructor) {
+    ASSERT_TRUE(true);
+}
